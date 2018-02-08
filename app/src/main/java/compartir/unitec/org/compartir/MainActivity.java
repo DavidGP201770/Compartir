3a9d804fbb47b8b450900d2887ff06484b40e016
DavidGP201770 <davicho201170@gmail.com>
2018-02-08 17:43:21 -0600

    EH WEEEEEEEEEEEE LOS LOCUSTS WEEEEEEEEEE ME SALIO EL TRASHBALL DRONE WEEEEEEEEEEEEEEEEEE LOS DE CUALITCHON ME LA P*L*N :3

--- app/src/main/java/compartir/unitec/org/compartir/MainActivity.java
+++ app/src/main/java/compartir/unitec/org/compartir/MainActivity.java
@@ -2,6 +2,9 @@ package compartir.unitec.org.compartir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Maldito locusts weeeeeeee
+
+        findViewById(R.id.Enviar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Tu mensaje se envio :3",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 }
