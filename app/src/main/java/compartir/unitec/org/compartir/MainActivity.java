0acc06cc7a6a8089a70d76ffff5c6f096553f483
DavidGP201770 <davicho201170@gmail.com>
2018-02-09 17:15:56 -0600

    x4 YA ANDO AQUI

--- app/src/main/java/compartir/unitec/org/compartir/MainActivity.java
+++ app/src/main/java/compartir/unitec/org/compartir/MainActivity.java
@@ -1,25 +1,28 @@
 package compartir.unitec.org.compartir;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText textoCuerpo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Maldito locusts weeeeeeee
 
-        findViewById(R.id.Enviar).setOnClickListener(new View.OnClickListener() {
+
+        textoCuerpo = (EditText) findViewById(R.id.mensaje);
+        findViewById(R.id.compartir).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Tu mensaje se envio :3",Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, textoCuerpo.getText().toString());
+                startActivity(Intent.createChooser(sharingIntent, "Enviando Mensaje"));
             }
         });
-
     }
 }
